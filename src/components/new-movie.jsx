8b874d148785e85a448d8c4bd81749b678c0c7ff
Joshua Javier <joshua.javier.github@gmail.com>
2021-07-18 11:37:13 +0800

    Deleted unused imports

--- src/components/new-movie.jsx
+++ src/components/new-movie.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
